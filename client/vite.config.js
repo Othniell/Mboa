150b2dcb1482e29c4d9650108324a607f20ce89f
Othniel.dev <113034727+Othniell@users.noreply.github.com>
2025-07-02 10:49:41 +0100

    good version

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,21 +8,28 @@ export default defineConfig({
     host: true, // allows access over local network
   },
   build: {
-  outDir: 'dist',
-  assetsDir: 'assets',
-  emptyOutDir: true,
-  chunkSizeWarningLimit: 1000,
-  rollupOptions: {
-    onwarn(warning, warn) {
-      console.warn("⚠️ Build Warning:", warning.message);
-      if (warning.code === 'CSS_SYNTAX_ERROR') {
-        console.log('❌ Possibly bad CSS chunk:', warning);
-      }
-      warn(warning);
-    },
-  },
-},
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1200, // raises limit from 500kB to avoid warning
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react-vendors';
+            if (id.includes('framer-motion')) return 'motion';
+            if (id.includes('react-icons')) return 'icons';
+            if (id.includes('leaflet')) return 'leaflet';
+          }
 
+          if (id.includes('/src/pages/')) return 'pages';
+          if (id.includes('/src/Components/')) return 'components';
+          if (id.includes('/src/context/')) return 'context';
+          if (id.includes('/src/utils/')) return 'utils';
+        }
+      }
+    }
+  },
   resolve: {
     alias:  {
       '@': '/src',
