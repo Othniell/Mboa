ba4cf5701efe3efaa6e1a432572f3c60fdefbf7b
Othniel.dev <113034727+Othniell@users.noreply.github.com>
2025-07-03 21:34:59 +0100

    good

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Mboa",
   server:  {
     port: 3000,
     host: true, // allows access over local network
